42ed245df569a4cd64fe66e71e0a2078fceaa39e
DEscutia <44597771+DEscutia@users.noreply.github.com>
2019-11-05 12:49:19 -0600

    Partials

---
+++ partials/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Funerales Izquierdo</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/jquery.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  </head>
+  <body>
+  <header>
+  <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+<a class="navbar-brand" href="index.php">
+    <img src="images\logo.jpg" width="30" height="30" alt="">
+    Funerales Izquierdo
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" 
+            data-target="#navbarMenu" aria-controls="navbarMenu" 
+            aria-expanded="false">
+            <span class="navbar-toggler-icon"></span>
+        </button>   
+  <div class="collapse navbar-collapse" id="navbarMenu">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="conocenos.php">Quienes Somos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="obituarios.php">Obituarios</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="ubicacion.php">Ubicación</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="muestrario.php">Muestrario</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</header>
